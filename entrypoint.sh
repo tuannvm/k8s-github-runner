f1d47263f65fba45a797c4eea79ee856832f22a8
Tommy Nguyen <tommy.nguyen@vungle.com>
2020-10-28 19:30:41 +0800

    Revert entrypoint
    Signed-off-by: Tommy Nguyen <tommy.nguyen@vungle.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 # shellcheck disable=SC2034
 # ACTIONS_RUNNER_INPUT_NAME is used by config.sh
 ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
+# get regsistration token for this runnner
+ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token" --header "authorization: Bearer ${GITHUB_TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
 # configure runner
 export RUNNER_ALLOW_RUNASROOT=1
-/runner/config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$RUNNER_TOKEN" --labels k8s-github-runner
+/runner/config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN" --labels k8s-github-runner
 # start runner
 # https://github.com/actions/runner/issues/246#issuecomment-615293718
 /runner/bin/runsvc.sh
